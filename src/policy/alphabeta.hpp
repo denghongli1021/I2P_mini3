494c23931e057e6abae24c35e11c5f8fba872d16
denghongli1021 <116105955+denghongli1021@users.noreply.github.com>
2023-06-19 14:09:40 +0800

    Update alphabeta.hpp

--- src/policy/alphabeta.hpp
+++ src/policy/alphabeta.hpp
@@ -1,13 +1,13 @@
-#pragma once
-#include "../state/state.hpp"
+// #pragma once
+// #include "../state/state.hpp"
 
 
-/**
- * @brief Policy class for random policy, 
- * your policy class should have get_move method
- */
-class AlpBe{
-public:
-  static int alphabeta(State& root ,int depth , bool maximizeplayer,int alpha,int beta);
-  static Move get_move(State *state, int depth);
-};
+// /**
+//  * @brief Policy class for random policy, 
+//  * your policy class should have get_move method
+//  */
+// class AlpBe{
+// public:
+//   static int alphabeta(State& root ,int depth , bool maximizeplayer,int alpha,int beta);
+//   static Move get_move(State *state, int depth);
+// };
